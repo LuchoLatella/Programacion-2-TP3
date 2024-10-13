b6688030c3a4e961eab3c5a546f1e1718d1f2241
LuchoLatella <lucho_latella@hotmail.com>
2024-10-13 17:28:11 -0300

    modifica clase pizaa

--- pizza.py
+++ pizza.py
@@ -1,13 +1,24 @@
 class Pizza:
+    ESTADO_POR_COCINAR = 1
+    ESTADO_COCINADA = 2
+    ESTADO_ENTREGADA = 3
 
-    def __init__(self, var: str):
+    def __init__(self, var: PizzaVariedad):
+        self.__variedad = var
+        self.__estado = Pizza.ESTADO_POR_COCINAR
+
+    def establecerVariedad(self, var: PizzaVariedad):
         self.__variedad = var
 
-    def establecerVariedad(self, var: str):
-        self.__variedad = var
+    def establecerEstado(self, est: int):
+        if est in [Pizza.ESTADO_POR_COCINAR, Pizza.ESTADO_COCINADA, Pizza.ESTADO_ENTREGADA]:
+            self.__estado = est
 
     def obtenerVariedad(self):
         return self.__variedad
-    
+
+    def obtenerEstado(self):
+        return self.__estado
+
     def __repr__(self):
-        return self.__variedad
+        return self.__variedad.obtenerNombreVariedad()
